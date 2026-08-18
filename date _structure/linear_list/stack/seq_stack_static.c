f13da0f90fd1596a71542fc6b4596f160516d113
huangweizhe76-blip <huangweizhe76@gmail.com>
2026-08-18 17:22:24 +0800

    implement origin static sequence stack

---
+++ date _structure/linear_list/stack/seq_stack_static.c
@@ -0,0 +1,88 @@
+﻿#include <stdio.h>
+
+#define MAXSIZE 100
+typedef int ElemType;
+
+typedef struct
+{
+	ElemType data[MAXSIZE];
+	int top;
+
+}Stack;
+
+//初始化
+void initStack(Stack* s)
+{
+	s->top = -1;
+}
+
+//判断栈是否为空
+int isEmpty(Stack* s)
+{
+	if (s->top == -1)
+	{
+		printf("空的\n");
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+//进栈/压栈
+int push(Stack* s, ElemType e)
+{
+	if (s->top >= MAXSIZE - 1)
+	{
+		printf("满了\n");
+		return 0;
+	}
+	s->top++;
+	s->data[s->top] = e;
+	return 1;
+}
+
+//出栈
+int pop(Stack* s, ElemType* e)
+{
+	if (s->top == -1)
+	{
+		printf("空的\n");
+		return 0;
+	}
+	*e = s->data[s->top];
+	s->top--;
+	return 1;
+}
+
+//获取栈顶元素
+int getTop(Stack* s, ElemType* e)
+{
+	if (s->top == -1)
+	{
+		printf("空的\n");
+		return 0;
+	}
+	*e = s->data[s->top];
+	return 1;
+}
+
+int main(int argc, char const* argv[])
+{
+
+	Stack s;
+	initStack(&s);
+	push(&s, 10);
+	push(&s, 20);
+	push(&s, 30);
+	ElemType e;
+	pop(&s, &e);
+	printf("%d\n", e);
+	getTop(&s, &e);
+	printf("%d\n", e);
+
+	return 0;
+}
+
+
