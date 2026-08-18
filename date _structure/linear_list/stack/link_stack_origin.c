5e966f26d29db482d3565f8d1bcee32e3ad5bedc
huangweizhe76-blip <huangweizhe76@gmail.com>
2026-08-18 17:22:43 +0800

    feat: implement origin linked stack(has known defects)

---
+++ date _structure/linear_list/stack/link_stack_origin.c
@@ -0,0 +1,91 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElemType;
+
+typedef struct stack
+{
+	ElemType data;
+	struct stack* next;
+}Stack;
+
+//初始化
+Stack* initStack()
+{
+	Stack* s = (Stack*)malloc(sizeof(Stack));
+	s->data = 0;
+	s->next = NULL;
+	return s;
+}
+
+//判断栈是否为空
+int isEmpty(Stack* s)
+{
+	if (s->next == NULL)
+	{
+		printf("空的\n");
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+//进栈/压栈
+int push(Stack* s, ElemType e)
+{
+	Stack* p = (Stack*)malloc(sizeof(Stack));
+	p->data = e;
+	p->next = s->next;
+	s->next = p;
+	return 1;
+}
+
+//出栈
+int pop(Stack* s, ElemType* e)
+{
+	if (s->next == NULL)
+	{
+		printf("空的\n");
+		return 0;
+	}
+	*e = s->next->data;
+	Stack* q = s->next;
+	s->next = q->next;
+	free(q);
+	return 1;
+}
+
+//获取栈顶元素
+int getTop(Stack* s, ElemType* e)
+{
+	if (s->next == NULL)
+	{
+		printf("空的\n");
+		return 0;
+	}
+	*e = s->next->data;
+	return 1;
+}
+
+int main(int argc, char const* argv[])
+{
+
+	Stack* s = initStack();
+	push(s, 10);
+	push(s, 20);
+	push(s, 30);
+
+	ElemType e;
+	pop(s, &e);
+	printf("%d\n", e);
+
+	getTop(s, &e);
+	printf("%d\n", e);
+
+	return 0;
+}
+
+
+
