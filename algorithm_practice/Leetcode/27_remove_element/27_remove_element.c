3e3d361e93b6c6d37bbc1514d28332fc08adcac2
huangweizhe76-blip <huangweizhe76@gmail.com>
2026-08-19 22:37:23 +0800

    feat: ac leetcode 27 remove‑element 移除元素

---
+++ algorithm_practice/Leetcode/27_remove_element/27_remove_element.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+
+// leetcode核心函数，和你截图逻辑完全一致
+int removeElement(int* nums, int numsSize, int val) {
+    int left = 0;
+    int right = numsSize - 1;
+    while (left <= right)
+    {
+        if (nums[left] == val)
+        {
+            nums[left] = nums[right];
+            right--;
+        }
+        else
+        {
+            left++;
+        }
+    }
+    return left;
+}
+
+int main()
+{
+    //测试样例 nums = [3,2,2,3], val=3
+    int nums[] = { 3,2,2,3 };
+    int val = 3;
+    int size = sizeof(nums) / sizeof(nums[0]);
+
+    int k = removeElement(nums, size, val);
+
+    printf("k = %d\n", k);
+    printf("数组前k个元素：");
+    for (int i = 0; i < k; i++)
+    {
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
+    return 0;
+}
